830ec37baffb4fb0646a99d3fb16ffbbff523d7b
dnd311 <doanngocduong03112006@gmail.com>
2026-01-04 20:09:14 +0700

    04-01-2026 Lesson 1: Setup Spring Tool Suite

---
+++ src/hoidanit/HelloWorld.java
@@ -0,0 +1,12 @@
+package hoidanit;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world Hỏi Dân IT");
+		System.out.println("Hello world Hỏi Dân IT");
+		System.out.println("Hello world Hỏi Dân IT");
+		System.out.println("Hello world Hỏi Dân IT");
+		System.out.println("Hello world Hỏi Dân IT");
+
+	}
+}
